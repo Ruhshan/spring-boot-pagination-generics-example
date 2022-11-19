6e7a2df3687ea5545a83463634cebee148de93b9
Ruhshan <ruhshan.ahmed@gmail.com>
2022-11-19 19:22:46 +0600

    Init project

---
+++ src/main/java/paginationexample/SpringBootPaginationExampleApplication.java
@@ -0,0 +1,13 @@
+package paginationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPaginationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPaginationExampleApplication.class, args);
+    }
+
+}
